c2de3948ca316154bf9392ee539f78c80a9a7e30
El Abror <luxamrownn@gmail.com>
2023-12-26 23:52:20 +0700

    Revert routes api php, change welcome word for trigger purpose

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
                 <div class="flex justify-center">
 
                     <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                        Hello TPM :)
+                        Hello TPM... :)
                     </p>
                 </div>
 
