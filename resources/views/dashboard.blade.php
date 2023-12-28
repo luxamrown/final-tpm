05b05e8afe53eac60165dda428ef7d60108a09d1
ShammyJindan <shammyjindan@gmail.com>
2023-12-28 11:46:06 +0700

    navbar and background color done

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,52 @@
+<style>
+    .background {
+        background: linear-gradient(to bottom, #FFF, #FFF, #BC87FF, #2F0CBD, #15025f);
+    }
+
+    .gap {
+        margin-right: 20px;
+    }
+</style>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>HACKTECH</title>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body>
-    <h1>HELLO</h1>
+<body class="background">
+    <nav class="navbar navbar-expand-lg navbar-light" style="margin-top: 10px;">
+        <a class="navbar-brand" href="#" style="margin-left: 100px; font-weight: bold; margin-right: 400px;">HACKTECH</a>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link gap" href="#" style="color:black; font-weight:200;">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link gap" href="#" style="color: black; font-weight:200;">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link gap" href="#" style="color: black; font-weight:200;">Champion & Prizes</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link gap" href="#" style="color: black; font-weight:200;">FAQ</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link gap" href="#" style="color: black; font-weight:200;">Timeline</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link gap" href="#" style="color: black; font-weight:200;">Mentor & Jury</a>
+            </li>
+            <li class="nav-item">
+                <button type="reset" id="loginButton" style="margin-top: 5px;">Login</button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <hr style="opacity: 100%; background-color:black; margin-left: 100px; margin-right: 110px; height: 3px; border-radius: 2px;">
 </body>
 </html>
