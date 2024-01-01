48055f3a5dd853d893edc35153e2d91434de232b
ShammyJindan <shammyjindan@gmail.com>
2024-01-01 12:23:55 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
     return view('dashboard');
-});
-
-Route::get('/dashboard2', function () {
-    return view('dashboard2');
 });
