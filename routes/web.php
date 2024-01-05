94b7e4d3ba766cd31b7af146d005199f04c6907d
DavinTanaya <davin.tanaya@gmail.com>
2024-01-05 18:57:03 +0700

    mekanisme login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/logout', [LoginController::class,'logout'])->name('logout');
+Route::get('/login', [LoginController::class,'index'])->name('login');
+Route::post('/login', [LoginController::class,'login'])->name('login');
+// Route::group(['middleware' => 'auth'], function () {
+//     Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
+//     Route::get('/logout', [LoginController::class,'logout'])->name('logout');
+// });
+
+// Route::group(['middleware' => 'guest'], function () {
+//     Route::get('/login', [LoginController::class,'index'])->name('login');
+//     Route::post('/login', [LoginController::class,'login'])->name('login');
+// });
