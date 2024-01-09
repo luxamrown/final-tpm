3fe826817ea877125c8b3aa77b2d05d48d1540ca
ShammyJindan <shammyjindan@gmail.com>
2024-01-09 12:00:32 +0700

    progress homepage 9 januari

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,6 +20,24 @@
         margin-bottom: 0px;
         letter-spacing: 3px;
     }
+
+    .background1, .background2 {
+            position: relative;
+            padding-bottom: 100px;
+        }
+
+        @media screen and (max-width: 768px) {
+            .whiteBox {
+                margin-top: 50px;
+            }
+        }
+
+        @media screen and (min-width: 769px) {
+            .whiteBox {
+                margin-top: 150px;
+            }
+        }
+
 </style>
 
 <!DOCTYPE html>
@@ -80,7 +98,7 @@
                         </p>
                         <button type="reset" class="poppins" id="register" style="border: #0D0043; margin-left: 60px; ">Register Here !</button>
                         <span>
-                            <img src="{{asset('frames/Ellipse 2.png')}}" style="position: absolute; right: 53%; width: 60px; height: 50px;">
+                            <img src="{{asset('frames/Ellipse 2.png')}}" style="position: relative; right: -6%; width: 55px; height: 50px; bottom: 6%;">
                         </span>
                     </div>
                     <div class="col" style="padding-right: 0px; position: relative">
@@ -98,29 +116,37 @@
                                 <p class="smallFont sty1">Sponsors</p>
                             </div>
                         </div>
-                        <img src="{{asset('frames/image 1.png')}}" style="width: 450px; height: 300px; position: absolute; bottom: -3%;">
+                        <img src="{{asset('frames/image 1.png')}}" style="width: 520px; height: 350px; position: absolute; bottom: -14%; right: 7%;">
                         <img src="{{asset('frames/Rectangle 2.png')}}" alt="" style="width: 486px; height: 620px; margin-left: 8px; z-index: 0;">
                     </div>
                 </div>
                 <img src="{{asset('frames/Frame1.png')}}" alt="" style="width: 230px; height: 200px; margin-top: 40px; margin-bottom: 130px; position: absolute; left: 30px;">
             </div>
         </div>
-        <img src="{{asset('frames/Ellipse 1.png')}}" style="z-index: 1; position: absolute; right: 0%; width: 750px; height: 600px; bottom: -45%;">
+        <img src="{{asset('frames/Ellipse 1.png')}}" style="z-index: 1; position: absolute; right: 0%; width: 800px; height: 600px; bottom: -8%;">
     </div>
     <div class="background2" style="height: 1500px;">
         <h1 class="poppins-bold" style="font-size: 130px; color:black; margin-top: 100px; margin-left: 100px; letter-spacing: 10px;"> JULY <span style="margin-left: 40px; letter-spacing: 0px; !important">28 - 30</span></h1>
         <h1 class="poppins-bold" style="font-size: 130px; color: black; margin-left: 100px; letter-spacing: 10px;">2023</h1>
-        <img src="{{asset('frames/image 2.png')}}" style="width: 830px; height: 550px; position: absolute; right: 5%; bottom: -128%; z-index: 1;">
+        <img src="{{asset('frames/image 2.png')}}" style="width: 830px; height: 550px; position: absolute; right: 5%; bottom: 51%; z-index: 1;">
         <img src="{{asset('frames/Ellipse 3.png')}}" style="position: absolute; left: 0%; width: 900px; height: 600px;">
-        <img src="{{asset('frames/Play button.png')}}" style="position: absolute; left: 23%; width: 165px; hieght: 167px; bottom: -107%;">
-        <div class="poppins" style="position:absolute; left: 6%; bottom: -125%;">
+        <img src="{{asset('frames/Play button.png')}}" style="position: absolute; left: 23%; width: 165px; height: 167px; bottom: 60%;">
+        <div class="poppins" style="position:absolute; left: 6%; bottom: 50%;">
             <h1 style="letter-spacing: 13px;  font-size: 70px; color:black;">Play <p style="margin-top: 17px;">Video</p></h1>
             <p class="smallFont poppins-light" style="margin-top: 30px; letter-spacing: 3px">VIDEO RECAP TECHNO</p>
         </div>
-        <img src="{{asset('frames/teamwork.png')}}" style="width: 600px; height: 900px; position: absolute; bottom: -253%; left: 7%;">
-        <img src="{{asset('frames/image 11.png')}}" style="width: 50px; height: 50px; position: absolute; bottom: -148%; left: 53%;">
-        <h1></h1>
-
+        <img src="{{asset('frames/teamwork.png')}}" style="width: 600px; height: 900px; position: absolute; bottom: -17%; left: 7%;">
+        <img src="{{asset('frames/image 11.png')}}" style="width: 50px; height: 50px; position: absolute; bottom: 40%; left: 53%;">
+        <div style="position: absolute; bottom: 15%; right: 9%; width: 490px;">
+            <h1 class="poppins" style="color:black; text-align:center; line-height: 1.5; font-weight: bold; margin-bottom: 30px;">
+                Don't wait ! Do something when you are young, when you have nothing to lose, and
+                keep that in mind
+            </h1>
+            <h3 class="poppins-light" style="color: black; text-align:center; font-style: italic;">- Steve Jobs</h3>
+        </div>
+        <img src="{{asset('frames/image 12.png')}}" style="width: 50px; height: 50px; position: absolute; bottom: 10%; left: 90%;">
+        <img src="{{asset('frames/Ellipse 4.png')}}" style="position:absolute; bottom: -30%; right: 0%; height: 500px;">
+        <img src="{{asset('frames/Frame 28.png')}}" style="position:absolute; bottom: -30%; right: 0%; height: 490px;">
     </div>
 </body>
 </html>
