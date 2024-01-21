e26dd44837ec64c20b7ba40b08bfc9f77bec1e41
El Abror <luxamrownn@gmail.com>
2024-01-21 20:21:57 +0700

    update web php

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,16 @@ Route::get('/', function () {
 });
 
 Route::POST('/register', [RegisterController::class, 'register'])->name('register');
+Route::get('/logout', [LoginController::class,'logout'])->name('logout');
+Route::get('/login', [LoginController::class,'index'])->name('login');
+Route::post('/login', [LoginController::class,'login'])->name('login');
+
 Route::GET('/adminpanel', [PanelController::class, 'getListTeam'])->name('getListTeam');
 Route::POST('/adminpanel', [PanelController::class, 'getDetailTeam'])->name('getDetailTeam');
 Route::PUT('/adminpanel', [PanelController::class, 'updateTeam'])->name('updateTeam');
 Route::DELETE('/adminpanel', [PanelController::class, 'deleteTeam'])->name('deleteTeam');
+
 Route::post('/send-mail', [MailController::class, 'sendMail' ])-> name('sendMail');
-Route::get('/logout', [LoginController::class,'logout'])->name('logout');
-Route::get('/login', [LoginController::class,'index'])->name('login');
-Route::post('/login', [LoginController::class,'login'])->name('login');
 // Route::group(['middleware' => 'auth'], function () {
 //     Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
 //     Route::get('/logout', [LoginController::class,'logout'])->name('logout');
