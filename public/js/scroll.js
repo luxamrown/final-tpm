5c1daf21089787b32d7d99a2b9a76e2fd0c83331
El Abror <luxamrownn@gmail.com>
2024-02-04 19:43:03 +0700

    update autoscroll menu footer

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -16,10 +16,22 @@ document.getElementById("aboutButton").addEventListener("click", function() {
     });
 });
 
+document.getElementById("aboutButton2").addEventListener("click", function() {
+  document.getElementById("aboutTarget").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
 document.getElementById("championButton").addEventListener("click", function() {
     document.getElementById("championTarget").scrollIntoView({
       behavior: "smooth"
     });
+});
+
+document.getElementById("championButton2").addEventListener("click", function() {
+  document.getElementById("championTarget").scrollIntoView({
+    behavior: "smooth"
+  });
 });
 
 document.getElementById("faqButton").addEventListener("click", function() {
@@ -28,16 +40,35 @@ document.getElementById("faqButton").addEventListener("click", function() {
     });
 });
 
+document.getElementById("faqButton2").addEventListener("click", function() {
+  document.getElementById("faqTarget").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+
 document.getElementById("timelineButton").addEventListener("click", function() {
     document.getElementById("timelineTarget").scrollIntoView({
       behavior: "smooth"
     });
 });
 
+document.getElementById("timelineButton2").addEventListener("click", function() {
+  document.getElementById("timelineTarget").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
 document.getElementById("mentorButton").addEventListener("click", function() {
     document.getElementById("mentorTarget").scrollIntoView({
       behavior: "smooth"
     });
+});
+
+document.getElementById("mentorButton2").addEventListener("click", function() {
+  document.getElementById("mentorTarget").scrollIntoView({
+    behavior: "smooth"
+  });
 });
 
 document.getElementById("homeButton").addEventListener("click", function() {
